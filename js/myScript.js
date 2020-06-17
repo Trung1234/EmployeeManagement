217c8fcd0a4db3c098e8b912839f18c7616d54b1
Trung <trungsieunhan24@gmail.com>
2020-06-17 12:33:59 +0700

    fix text

--- js/myScript.js
+++ js/myScript.js
@@ -1,44 +1,4 @@
 
-function addRow(){
-    resetError();
-    var tableRows = $('#employee tr').length;
-    var name = $("#name").val();
-    var classOfStudent = $("#class").val();
-    var math = Number($("#math").val());
-    var physic = Number($("#physic").val());
-    var chemical = Number($("#chemical").val());
-    if(name.length==0){
-        $("#nameError").css("color", "red").text("Họ tên bắt buộc phải nhập");
-    }else{
-        if(classOfStudent.length==0){       
-            $("#classError").css("color", "red").text("Lớp học bắt buộc phải nhập");
-        }else{
-            if(math.length==0 || math <= 0 || math > 10){
-                $("#mathError").css("color", "red").text("Điểm toán bắt buộc phải nhập và phải trong khoảng từ 0 đến 10");
-            }else{
-                if(physic.length==0 || physic <= 0 || physic > 10){
-                    $("#physicError").css("color", "red").text("Điểm lý bắt buộc phải nhập và phải trong khoảng từ 0 đến 10");
-                }else{
-                    if(chemical.length==0 || chemical <= 0 || chemical > 10){
-                        $("#chemicalError").css("color", "red").text("Điểm hóa bắt buộc phải nhập và phải trong khoảng từ 0 đến 10");
-                    }else{
-                        $(document).ready(function () {
-                            $('#employee').append('<tr id="row'+tableRows+'"><td>'+tableRows+
-                            '</td><td style="text-align:left;">'+name +'</td>'
-                            +'<td>' +classOfStudent +'</td>'
-                            +'<td class="math">'
-                            +math
-                            +'</td><td class="physic">'+physic
-                            +'</td><td class="chemical">'+chemical
-                            +'</td><td class="average">'+'?'+
-                            '</td></tr>');
-                        });
-                    }
-                }
-            }
-        }
-    }    
-}
 function addEmployee(){
     resetError();
     var isValid = true;
